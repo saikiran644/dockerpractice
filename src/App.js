2f2bdcadeabb3188e1fcf0a4cc4c7e952cd9b6c8
sai <saikiran644@gmail.com>
2020-01-06 12:53:30 +0530

    added changes for the demo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited from the feature branch.
+          Changes made for the demo
         </p>
         <a
           className="App-link"
